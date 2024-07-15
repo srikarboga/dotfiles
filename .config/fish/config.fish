a1a724efdd8216e84601996ec73e9d9f283746ee
Srikar Boggavarapu <117547124+srikarboga@users.noreply.github.com>
2024-07-15 16:16:13 -0400

    shell setup

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,20 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+function fish_greeting
+	if test "$TERM" = "xterm-kitty"
+		set_color blue
+		echo - $hostname (date +'%I:%M:%S %p')
+		set_color cyan
+		echo - (uptime -p)
+	end
 end
+
+function fish_prompt
+    set_color green
+    echo -n (whoami)
+    set_color normal
+    echo -n "@"(hostname)" "
+    set_color green
+    echo -n (prompt_pwd)
+    set_color normal
+    echo -n "\$ "
+end
+
