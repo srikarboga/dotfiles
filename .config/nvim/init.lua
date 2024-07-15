9758f2c972434f0dd01bd01783cdbd9384ff4c26
Srikar Boggavarapu <117547124+srikarboga@users.noreply.github.com>
2024-07-15 15:09:34 -0400

    Added tabstop settings from primeagen vid

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -154,6 +154,12 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Primeagen tab settings copied
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
