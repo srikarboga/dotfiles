4f13c4ecf629b848176b325d1cbbacee68c03647
srikarboga <boggavarapu.srikar@gmail.com>
2024-08-05 18:17:31 -0400

    minor tweaks

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -705,6 +705,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        html = { 'prettier' },
+        css = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
